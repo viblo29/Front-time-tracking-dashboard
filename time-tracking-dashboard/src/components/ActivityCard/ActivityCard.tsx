b4a9386c3fb5a9b5df953c73f173c0ff83834782
viblo29 <dachivibliani29@gmail.com>
2025-11-17 03:14:30 +0400

    fixed mobile responsive problem

--- time-tracking-dashboard/src/components/ActivityCard/ActivityCard.tsx
+++ time-tracking-dashboard/src/components/ActivityCard/ActivityCard.tsx
@@ -45,24 +45,24 @@ function ActivityCard({
     <>
       <div className="flex flex-col w-[327px] h-[160px] lg:w-[255px] lg:h-[244px] z-0 relative rounded-[15px]">
         <img
-          className="sm:w-[100%] object-contain lg:w-[255px] h-[160px] absolute top-0 z-1"
+          className="w-[100%] object-contain lg:w-[255px] h-[160px] absolute top-0 z-1"
           src={imageUrl}
           alt={`Icon for ${title}`}
         />
         
-        <div className="flex gap-[3px] cursor-pointer group absolute sm:top-[75px] lg:top-[82px] sm:right-[24px] lg:right-[30px] z-3">
+        <div className="flex gap-[3px] cursor-pointer group absolute top-[75px] lg:top-[82px] right-[24px] lg:right-[30px] z-3">
           <div className="rounded-full w-[5px] h-[5px] bg-[#BBC0FF] group-hover:bg-[#FFFFFF] transition-colors duration-300"></div>
           <div className="rounded-full w-[5px] h-[5px] bg-[#BBC0FF] group-hover:bg-[#FFFFFF] transition-colors duration-300"></div>
           <div className="rounded-full w-[5px] h-[5px] bg-[#BBC0FF] group-hover:bg-[#FFFFFF] transition-colors duration-300"></div>
         </div>
 
-        <div className="flex flex-col sm:gap-[6px] lg:gap-[24px] w-full sm:h-[122px] lg:h-[199px] bg-[#1C204B] absolute bottom-0 z-2 rounded-[15px] cursor-pointer hover:bg-[#33397A] pt-[29px] sm:px-[24px] lg:px-[30px] text-[#FFFFFF] transition-colors duration-300">
+        <div className="flex flex-col gap-[6px] lg:gap-[24px] w-full h-[122px] lg:h-[199px] bg-[#1C204B] absolute bottom-0 z-2 rounded-[15px] cursor-pointer hover:bg-[#33397A] pt-[29px] px-[24px] lg:px-[30px] text-[#FFFFFF] transition-colors duration-300">
             <div className="flex justify-between items-center text-[18px]/[18px]">
                 <div>{title}</div>
             </div>
 
-            <div className="flex sm:flex-row sm:justify-between sm:items-center lg:items-start lg:flex-col gap-[8px]">
-                <div className="sm:text-[32px] lg:text-[56px]/[56px] font-light">
+            <div className="flex flex-row justify-between items-center lg:items-start lg:flex-col gap-[8px]">
+                <div className="text-[32px] lg:text-[56px]/[56px] font-light">
                     {time}hrs
                 </div>
                 <div className="text-[15px] text-[#BBC0FF]">
--- time-tracking-dashboard/src/components/UserProfileCard/UserProfileCard.tsx
+++ time-tracking-dashboard/src/components/UserProfileCard/UserProfileCard.tsx
@@ -1,8 +1,8 @@
 function UserProfileCard() {
   return (
     <>
-      <div className="sm:w-[327px] sm:h-[203px] sm:flex-col lg:flex-row lg:w-auto lg:h-[244px] xl:w-[255px] xl:h-[518px] flex xl:flex-col bg-[#1C204B] rounded-[15px]">
-          <div className="sm:h-[133px] sm:items-center xl:items-start lg:pt-0 lg:w-[50%] lg:h-[244px] xl:w-full xl:h-[354px] flex xl:flex-col sm:gap-[20px] lg:gap-[43px] rounded-[15px] bg-[#5747EA] xl:pt-[37px] pl-[32px] ">
+      <div className="w-[327px] h-[203px] flex-col lg:flex-row lg:w-auto lg:h-[244px] xl:w-[255px] xl:h-[518px] flex xl:flex-col bg-[#1C204B] rounded-[15px]">
+          <div className="h-[133px] items-center xl:items-start lg:pt-0 lg:w-[50%] lg:h-[244px] xl:w-full xl:h-[354px] flex xl:flex-col gap-[20px] lg:gap-[43px] rounded-[15px] bg-[#5747EA] xl:pt-[37px] pl-[32px] ">
             <img
               src="/user-profile-black-man-smiling.svg"
               alt="User profile"
@@ -12,13 +12,13 @@ function UserProfileCard() {
               <div className="text-[15px] text-[#BBC0FF] leading-none">
                 Report for
               </div>
-              <div className="sm:text-[24px] lg:text-[40px]/[41px] text-[#FFFFFF] lg:w-[138px] lg:font-light">
+              <div className="text-[24px] lg:text-[40px]/[41px] text-[#FFFFFF] lg:w-[138px] lg:font-light">
                 Jeremy Robson
               </div>
             </div>
           </div>
 
-          <div className="flex sm:justify-around sm:pt-[24px] lg:items-start sm:pl-0 lg:flex-col xl:gap-[15px] text-[#7078C9] lg:pl-[32px] lg:pt-[26px] text-[18px]">
+          <div className="flex justify-around pt-[24px] lg:items-start pl-0 lg:flex-col xl:gap-[15px] text-[#7078C9] lg:pl-[32px] lg:pt-[26px] text-[18px]">
             <div className="cursor-pointer hover:text-white transition-colors duration-300">Daily</div>
             <div className="text-[#FFFFFF] cursor-pointer hover:text-white transition-colors duration-300">Weekly</div>
             <div className="cursor-pointer hover:text-white transition-colors duration-300">Monthly</div>
