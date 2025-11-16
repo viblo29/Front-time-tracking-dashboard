866722b035ce6677ce1232400d36b1551fc6952a
viblo29 <dachivibliani29@gmail.com>
2025-11-17 03:24:37 +0400

    fixed svgs problem

--- time-tracking-dashboard/src/components/UserProfileCard/UserProfileCard.tsx
+++ time-tracking-dashboard/src/components/UserProfileCard/UserProfileCard.tsx
@@ -2,27 +2,33 @@ function UserProfileCard() {
   return (
     <>
       <div className="w-[327px] h-[203px] flex-col lg:flex-row lg:w-auto lg:h-[244px] xl:w-[255px] xl:h-[518px] flex xl:flex-col bg-[#1C204B] rounded-[15px]">
-          <div className="h-[133px] items-center xl:items-start lg:pt-0 lg:w-[50%] lg:h-[244px] xl:w-full xl:h-[354px] flex xl:flex-col gap-[20px] lg:gap-[43px] rounded-[15px] bg-[#5747EA] xl:pt-[37px] pl-[32px] ">
-            <img
-              src="/user-profile-black-man-smiling.svg"
-              alt="User profile"
-              className="rounded-full w-[78px] h-[78px] object-center"
-            />
-            <div className="flex flex-col gap-[3px]">
-              <div className="text-[15px] text-[#BBC0FF] leading-none">
-                Report for
-              </div>
-              <div className="text-[24px] lg:text-[40px]/[41px] text-[#FFFFFF] lg:w-[138px] lg:font-light">
-                Jeremy Robson
-              </div>
+        <div className="h-[133px] items-center xl:items-start lg:pt-0 lg:w-[50%] lg:h-[244px] xl:w-full xl:h-[354px] flex xl:flex-col gap-[20px] lg:gap-[43px] rounded-[15px] bg-[#5747EA] xl:pt-[37px] pl-[32px] ">
+          <img
+            src="/user-profile-black-man-smiling.svg"
+            alt="User profile"
+            className="rounded-full w-[78px] h-[78px] object-center"
+          />
+          <div className="flex flex-col gap-[3px]">
+            <div className="text-[15px] text-[#BBC0FF] leading-none">
+              Report for
+            </div>
+            <div className="text-[24px] lg:text-[40px]/[41px] text-[#FFFFFF] lg:w-[138px] lg:font-light">
+              Jeremy Robson
             </div>
           </div>
+        </div>
 
-          <div className="flex justify-around pt-[24px] lg:items-start pl-0 lg:flex-col xl:gap-[15px] text-[#7078C9] lg:pl-[32px] lg:pt-[26px] text-[18px]">
-            <div className="cursor-pointer hover:text-white transition-colors duration-300">Daily</div>
-            <div className="text-[#FFFFFF] cursor-pointer hover:text-white transition-colors duration-300">Weekly</div>
-            <div className="cursor-pointer hover:text-white transition-colors duration-300">Monthly</div>
+        <div className="flex justify-around pt-[24px] lg:items-start pl-0 lg:flex-col xl:gap-[15px] text-[#7078C9] lg:pl-[32px] lg:pt-[26px] text-[18px]">
+          <div className="cursor-pointer hover:text-white transition-colors duration-300">
+            Daily
           </div>
+          <div className="text-[#FFFFFF] cursor-pointer hover:text-white transition-colors duration-300">
+            Weekly
+          </div>
+          <div className="cursor-pointer hover:text-white transition-colors duration-300">
+            Monthly
+          </div>
+        </div>
       </div>
     </>
   );
